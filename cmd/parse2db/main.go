1c3f261f29c0a367285ab84300a15a18d288f48d
Douglas Fischer <anakin_skryter@hotmail.se>
2024-03-07 21:37:15 +0100

    reworked project structure

--- cmd/parse2db/main.go
+++ cmd/parse2db/main.go
@@ -1,15 +1,15 @@
-package main
+package parse2db
 
 import (
 	"fmt"
-	"github.com/datasektionen/GOrdian/internal/parse"
+	"github.com/datasektionen/GOrdian/internal/excel"
 	"log"
 )
 
 func main() {
 	fmt.Println("You have very many money")
 	budget := "test/Budget_2024.xlsx"
-	err := parse.ReadExcel(budget)
+	err := excel.ReadExcel(budget)
 	if err != nil {
 		log.Printf("error parsing Excel file: %v", err)
 	}
--- internal/excel/parser.go
+++ internal/excel/parser.go
@@ -1,4 +1,4 @@
-package parse
+package excel
 
 import (
 	"fmt"

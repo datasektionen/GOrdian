cb4b7e0d8a66a6cf26ff4ade1d6d534424ded1b5
Douglas Fischer <anakin_skryter@hotmail.se>
2024-03-07 22:30:31 +0100

    add db connect functionality to postgres.go

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -1,5 +1,41 @@
 package database
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"github.com/datasektionen/GOrdian/internal/config"
+	_ "github.com/lib/pq"
+)
 
+func Connect() (*sql.DB, error) {
+
+	envVar := config.GetEnv()
+
+	psqlconnString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		envVar.DBHost, envVar.DBPort, envVar.DBUser, envVar.DBPass, envVar.DBName)
+
+	db, err := sql.Open("postgres", psqlconnString)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %v", err)
+	}
+
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			_ = fmt.Errorf("error closing connection to database: %v", err)
+		}
+	}(db)
+
+	err = db.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("failed to ping database: %v", err)
+	}
+
+	fmt.Println("Connected to database")
+
+	return db, nil
+}
+
+func Close() error {
+	return nil
 }

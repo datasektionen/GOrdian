275dbc0a5dbce631a68611f354a87fcb1162885e
Douglas Fischer <anakin_skryter@hotmail.se>
2024-03-21 20:09:36 +0100

    added functionality for displaying cost centres

--- cmd/web/web.go
+++ cmd/web/web.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"github.com/datasektionen/GOrdian/internal/database"
+	"log"
 	"net/http"
 
 	"github.com/datasektionen/GOrdian/internal/web"
 )
 
 func main() {
-	if err := web.Mount(http.DefaultServeMux); err != nil {
+	db, err := database.Connect()
+	if err != nil {
+		log.Printf("error accessing database: %v", err)
+	}
+	if err := web.Mount(http.DefaultServeMux, db); err != nil {
 		panic(err)
 	}
 	panic(http.ListenAndServe("0.0.0.0:3000", nil))
--- internal/web/urls.go
+++ internal/web/urls.go
@@ -1,8 +1,10 @@
 package web
 
 import (
+	"database/sql"
 	"embed"
 	"fmt"
+	"github.com/datasektionen/GOrdian/internal/excel"
 	"html/template"
 	"log/slog"
 	"net/http"
@@ -13,21 +15,21 @@ var templatesFS embed.FS
 
 var templates *template.Template
 
-func Mount(mux *http.ServeMux) error {
+func Mount(mux *http.ServeMux, db *sql.DB) error {
 	var err error
 	templates, err = template.ParseFS(templatesFS, "templates/*.html")
 	if err != nil {
 		return err
 	}
 
-	mux.Handle("/{$}", page(index))
+	mux.Handle("/{$}", page(db, index))
 
 	return nil
 }
 
-func page(handler func(w http.ResponseWriter, r *http.Request) error) http.Handler {
+func page(db *sql.DB, handler func(w http.ResponseWriter, r *http.Request, db *sql.DB) error) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		err := handler(w, r)
+		err := handler(w, r, db)
 		if err != nil {
 			slog.Error("Error from handler", "error", err)
 			w.WriteHeader(500)
@@ -36,9 +38,60 @@ func page(handler func(w http.ResponseWriter, r *http.Request) error) http.Handl
 	})
 }
 
-func index(w http.ResponseWriter, r *http.Request) error {
-	if err := templates.ExecuteTemplate(w, "index.html", map[string]any{"motd": "You have very many money"}); err != nil {
+func index(w http.ResponseWriter, r *http.Request, db *sql.DB) error {
+	costCentres, err := getCostCentres(db)
+	if err != nil {
+		return fmt.Errorf("failed get scan cost centre information from database: %v", err)
+	}
+	committeeCostCentres, projectCostCentres, otherCostCentres, err := splitCostCentresOnType(costCentres)
+	if err != nil {
+		return fmt.Errorf("failed spit cost centres on type: %v", err)
+	}
+	if err := templates.ExecuteTemplate(w, "index.html", map[string]any{
+		"motd":       "You have very many money",
+		"committees": committeeCostCentres,
+		"projects":   projectCostCentres,
+		"others":     otherCostCentres,
+	}); err != nil {
 		return fmt.Errorf("Could not render template: %w", err)
 	}
 	return nil
+}
+
+func getCostCentres(db *sql.DB) ([]excel.CostCentre, error) {
+	var costCentresGetStatementStatic = `SELECT id, name, type FROM cost_centres`
+	result, err := db.Query(costCentresGetStatementStatic)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get cost centre from database: %v", err)
+	}
+	var costCentres []excel.CostCentre
+	for result.Next() {
+		var costCentre excel.CostCentre
+
+		err := result.Scan(&costCentre.CostCentreID, &costCentre.CostCentreName, &costCentre.CostCentreType)
+		if err != nil {
+			return nil, fmt.Errorf("failed to scan cost centre from query result: %v", err)
+		}
+		costCentres = append(costCentres, costCentre)
+	}
+	return costCentres, nil
+}
+
+func splitCostCentresOnType(costCentres []excel.CostCentre) ([]excel.CostCentre, []excel.CostCentre, []excel.CostCentre, error) {
+	var committeeCostCentres []excel.CostCentre
+	var projectCostCentres []excel.CostCentre
+	var otherCostCentres []excel.CostCentre
+	for _, costCentre := range costCentres {
+		switch costCentre.CostCentreType {
+		case "committee":
+			committeeCostCentres = append(committeeCostCentres, costCentre)
+		case "project":
+			projectCostCentres = append(projectCostCentres, costCentre)
+		case "other":
+			otherCostCentres = append(otherCostCentres, costCentre)
+		default:
+			return nil, nil, nil, fmt.Errorf("faulty cost centre type found when splitting")
+		}
+	}
+	return committeeCostCentres, projectCostCentres, otherCostCentres, nil
 }

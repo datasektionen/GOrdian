cb8e73da436746e13c3a4a441534d58b1a6d0b16
Douglas Fischer <anakin_skryter@hotmail.se>
2024-05-02 21:47:00 +0200

    update envconfig

--- internal/config/env.go
+++ internal/config/env.go
@@ -1,5 +1,7 @@
 package config
 
+import "os"
+
 type EnvVar struct {
 	DBHost     string
 	DBPort     string
@@ -17,18 +19,31 @@ type EnvVar struct {
 func GetEnv() EnvVar {
 
 	envConfig := EnvVar{
-		DBHost:     "localhost",
-		DBPort:     "5432",
-		DBUser:     "alexander",
-		DBPass:     "kopis",
-		DBName:     "budget_local",
-		LoginURL:   "https://login.datasektionen.se",
-		LoginToken: "",
-		PlsURL:     "https://pls.datasektionen.se",
-		PlsSystem:  "gordian",
-		ServerPort: "3000",
-		ServerURL:  "http://localhost:3000",
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPass:     os.Getenv("DB_PASS"),
+		DBName:     os.Getenv("DB_NAME"),
+		LoginURL:   os.Getenv("LOGIN_URL"),
+		LoginToken: os.Getenv("LOGIN_TOKEN"),
+		PlsURL:     os.Getenv("PLS_URL"),
+		PlsSystem:  os.Getenv("PLS_SYSTEM"),
+		ServerPort: os.Getenv("SERVER_PORT"),
+		ServerURL:  os.Getenv("SERVER_URL"),
 	}
 
 	return envConfig
 }
+
+//example
+//		DBHost:     localhost,
+//		DBPort:     "5432",
+//		DBUser:     "alexander",
+//		DBPass:     "kopis",
+//		DBName:     "budget_local",
+//		LoginURL:   "https://login.datasektionen.se",
+//		LoginToken: "this is secret fuck you",
+//		PlsURL:     "https://pls.datasektionen.se",
+//		PlsSystem:  "gordian",
+//		ServerPort: "3000",
+//		ServerURL:  "http://localhost:3000",

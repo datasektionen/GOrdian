39489f8f1b6d441cae3dc8338cdccdfea2f8ec5f
Douglas Fischer <anakin_skryter@hotmail.se>
2024-09-29 15:34:04 +0200

    updated env variables

--- internal/config/env.go
+++ internal/config/env.go
@@ -1,5 +1,7 @@
 package config
 
+import "os"
+
 type EnvVar struct {
 	PsqlconnStringGOrdian  string
 	PsqlconnStringCashflow string
@@ -15,24 +17,24 @@ func GetEnv() EnvVar {
 
 	envConfig := EnvVar{
 		//prod
-		//PsqlconnStringGOrdian:   os.Getenv("GO_CONN"),
-		//PsqlconnStringCashflow: os.Getenv("CF_CONN"),
-		//LoginURL:   os.Getenv("LOGIN_URL"),
-		//LoginToken: os.Getenv("LOGIN_TOKEN"),
-		//PlsURL:     os.Getenv("PLS_URL"),
-		//PlsSystem:  os.Getenv("PLS_SYSTEM"),
-		//ServerPort: os.Getenv("SERVER_PORT"),
-		//ServerURL:  os.Getenv("SERVER_URL"),
+		PsqlconnStringGOrdian:  os.Getenv("GO_CONN"),
+		PsqlconnStringCashflow: os.Getenv("CF_CONN"),
+		LoginURL:               os.Getenv("LOGIN_URL"),
+		LoginToken:             os.Getenv("LOGIN_TOKEN"),
+		PlsURL:                 os.Getenv("PLS_URL"),
+		PlsSystem:              os.Getenv("PLS_SYSTEM"),
+		ServerPort:             os.Getenv("SERVER_PORT"),
+		ServerURL:              os.Getenv("SERVER_URL"),
 
-		//local
-		PsqlconnStringGOrdian:  "host=localhost port=5432 user=alexander password=kopis dbname=budget_local sslmode=disable",
-		PsqlconnStringCashflow: "host=localhost port=54321 user=cashflow password=cashflow dbname=cashflow sslmode=disable",
-		LoginURL:               "https://login.datasektionen.se",
-		PlsURL:                 "https://pls.datasektionen.se",
-		PlsSystem:              "gordian",
-		ServerPort:             "3000",
-		ServerURL:              "http://localhost:3000",
-		LoginToken:             "this is secret fuck you",
+		// local
+		// PsqlconnStringGOrdian:  "host=localhost port=5432 user=alexander password=kopis dbname=budget_local sslmode=disable",
+		// PsqlconnStringCashflow: "host=localhost port=54321 user=cashflow password=cashflow dbname=cashflow sslmode=disable",
+		// LoginURL:               "https://login.datasektionen.se",
+		// PlsURL:                 "https://pls.datasektionen.se",
+		// PlsSystem:              "gordian",
+		// ServerPort:             "3000",
+		// ServerURL:              "http://localhost:3000",
+		// LoginToken:             "this is secret fuck you",
 	}
 
 	return envConfig

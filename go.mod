a66458eb817eec7086e071f49c7c310f7a21cddd
Mathias Magnusson <mathias@magnusson.space>
2025-01-16 18:58:02 +0100

    Add migrations with golang-migrate

--- go.mod
+++ go.mod
@@ -3,17 +3,21 @@ module github.com/datasektionen/GOrdian
 go 1.22.0
 
 require (
+	github.com/golang-migrate/migrate/v4 v4.18.1
 	github.com/lib/pq v1.10.9
 	github.com/xuri/excelize/v2 v2.8.1
 )
 
 require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/richardlehane/mscfb v1.0.4 // indirect
 	github.com/richardlehane/msoleps v1.0.3 // indirect
 	github.com/xuri/efp v0.0.0-20231025114914-d1ff6096ae53 // indirect
 	github.com/xuri/nfp v0.0.0-20230919160717-d98342af3f05 // indirect
-	golang.org/x/crypto v0.19.0 // indirect
-	golang.org/x/net v0.21.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 )
--- main.go
+++ main.go
@@ -10,13 +10,38 @@ import (
 	"github.com/datasektionen/GOrdian/internal/database"
 
 	"github.com/datasektionen/GOrdian/internal/web"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 )
+
+//go:embed migrations/*.sql
+var migrations embed.FS
 
 func main() {
 	envVar := config.GetEnv()
 	dbGO, err := database.Connect(envVar.PsqlconnStringGOrdian)
 	if err != nil {
 		log.Printf("error accessing GOrdian database: %v", err)
+	}
+
+	{
+		driver, err := postgres.WithInstance(dbGO, &postgres.Config{})
+		if err != nil {
+			panic(fmt.Errorf("Failed to migrate 1: %v", err))
+		}
+		migrations, err := iofs.New(migrations, "migrations")
+		if err != nil {
+			panic(fmt.Errorf("Failed to migrate 2: %v", err))
+		}
+		migrator, err := migrate.NewWithInstance("iofs", migrations, "gordian-db", driver)
+		if err != nil {
+			panic(fmt.Errorf("Failed to migrate 3: %v", err))
+		}
+		if err := migrator.Up(); err != migrate.ErrNoChange && err != nil {
+			panic(fmt.Errorf("Failed to migrate 4: %v", err))
+		}
 	}
 
 	dbCF, err := database.Connect(envVar.PsqlconnStringCashflow)
